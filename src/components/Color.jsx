c3b6235405a790e9e0fcc1856b5b68f14dac2616
saeid <saeid.rohparvar@gmail.com>
2023-09-08 17:37:40 +0330

    refactor color

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,65 +1,34 @@
-const colorPrimary = {
-  Primary_10: "#EDEFFF",
-  Primary_40: "#B6BDFF",
-  Primary_60: "#919DFF",
-  Primary_80: "#6D7CFF",
-  Primary_100: "#485BFF",
-  Primary_120: "#3A49CC",
-  Primary_140: "#2B3799",
-  Primary_160: "#1D2466",
-  Primary_190: "#07091A",
+const ColorPrimary = {
+  primary: "#F78F42",
+  primary_focus: "#EA7C2B",
+  primary_content: "#FFFFFF",
+};
+const ColorSecondary = {
+  secondary: "rgba(2, 6, 23, 0.60)",
+  secondary_focus: "#3B3C4A",
+  secondary_content: "#FFFFFF",
+};
+const ColorAccent = {
+  accent: "#3CC288",
+  accent_focus: "#33A574",
+  accent_content: "#FFFFFF",
+};
+const ColorNeutral = {
+  neutral: "#020617",
+  neutral_focus: "#141624",
+  neutral_content: "#FFFFFF",
+};
+const ColorBase = {
+  base_100: "#FFFFFF",
+  base_200: "#F6F6F7",
+  base_300: "rgba(2, 6, 23, 0.03)",
+  base_content:"#020617"
 };
 
-const Neutral = {
-  Neutral: "#FFFFFF",
-  Neutral_05: "#F3F3F3",
-  Neutral_10: "#E7E7E7",
-  Neutral_20: "#D0D0D0",
-  Neutral_40: "#A0A0A0",
-  Neutral_60: "#717171",
-  Neutral_80: "#414141",
-  Neutral_90: "#2A2A2A",
-  Neutral_95: "#1E1E1E",
-  Neutral_100: "#121212",
+export default {
+  ColorPrimary,
+  ColorSecondary,
+  ColorAccent,
+  ColorNeutral,
+  ColorBase,
 };
-
-const TextColor = {
-  // Text Light
-  Primary_Light: "#121212",
-  Secondary_Light: "#414141",
-  Tertiary_Light: "#717171",
-  LinkInfo_Light: "#485BFF",
-  // Text Dark
-  Primary_Dark: "#FFFFFF",
-  Secondary_Dark: "#D0D0D0",
-  Tertiary_Dark: "#A0A0A0",
-  LinkInfo_Dark: "#B6BDFF",
-  // UI Elements Light
-  Primary_Element_L: "#717171",
-  Secondary_Element_L: "#D0D0D0",
-  Tertiary_Element_L: "#E7E7E7",
-  // UI Elements Dark
-  Primary_Element_D: "#D0D0D0",
-  Secondary_Element_D: "#717171",
-  Tertiary_Element_D: "#414141",
-};
-
-const Background = {
-    Bg_Light:"#FFFFFF",
-    Bg_Dark:"#121212"
-}
-
-const IconColor = {
-    //Light
-    iconDefault_L : "#2A2A2A",
-    iconDisabled_L:"#A0A0A0",
-    iconActive:"#FFFFFF",
-    iconPrimaryContainer :"#485BFF",
-    //Dark
-    iconDefault_D : "#E7E7E7",
-    iconDisabled_D:"#717171",
-    iconActive_D:"#121212",
-    iconPrimaryContainer_D :"#B6BDFF"
-}
-
-export default { colorPrimary, Neutral, TextColor,Background,IconColor};
