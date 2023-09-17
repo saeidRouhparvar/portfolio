50c104887a4123161f0ed00fa43b6897473bf52b
saeid <saeid.rohparvar@gmail.com>
2023-09-17 23:42:23 +0330

    complate navbar

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -4,12 +4,14 @@ import Color from "../Color";
 import { pxToRem } from "../Utils";
 import Setting from "./Setting";
 import BurgerMenu from "./BurgerMenu";
+import Typography from "../Typography";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { colorMode } = useColorMode();
   return (
     <Box
-      display={{ base: "flex", xl: "none" }}
+      display={"flex"}
       justifyContent={"space-between"}
       mb={pxToRem(32)}
       bg={colorMode === "light" ? Color.ColorBase.base_100 : ""}
@@ -18,11 +20,23 @@ const Navbar = () => {
       px={pxToRem(24)}
       cursor={"pointer"}
     >
-        <Box><Avatar w={pxToRem(36)} h={pxToRem(36)}/></Box>
-        <Flex align={"center"} gap={{base:pxToRem(12),md:pxToRem(24)}}>
-            <Setting/>
-            <BurgerMenu/>
-        </Flex>
+      <Box>
+        <Avatar w={pxToRem(36)} h={pxToRem(36)} />
+      </Box>
+      <Box {...Typography.b4_regular} alignItems={"center"}
+      display={{ base: "none", xl: "flex" }}
+      gap={pxToRem(32)}>
+        <Link to={'/app'}>Home</Link>
+        <Link to={'/about'}>About</Link>
+        <Link to={'/services'}>Services</Link>
+        <Link to={'/projects'}>Projects</Link>
+        <Link to={'/blog'}>Blog</Link>
+        <Link to={'/contact'}>Contact</Link>
+      </Box>
+      <Flex align={"center"} gap={{ base: pxToRem(12), md: pxToRem(24) }}>
+        <Setting />
+        <BurgerMenu />
+      </Flex>
     </Box>
   );
 };
