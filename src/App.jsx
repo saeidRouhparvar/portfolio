fc3dd9ed53ba74478c795c6aca24ce3d9e076d81
saeid <saeid.rohparvar@gmail.com>
2023-09-17 23:05:24 +0330

    add responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { Routes, Route} from "react-router-dom";
+import { ChakraProvider, Box, Flex, useColorMode } from "@chakra-ui/react";
+import SideBar from "./components/sidBar/SideBar";
+import Color from "./components/Color";
+import { pxToRem } from "./components/Utils";
+import MainSection from "./components/mainSection/MainSection";
 import Navbar from "./components/header/Navbar";
-// import { indexRoutes } from "./routes";
-import Introduction from "./components/header/Introduction";
-import Projects from "./components/project/Projects";
-import Experiences from "./components/experience/Experiences";
 
 const App = () => {
+  // const {colorMode} = useColorMode()
   return (
     <ChakraProvider>
-      {/* <Routes> */}
-        {/* {indexRoutes.map((index,key) => {
-          return (
-            <Route path={index.path} key={key} />
-          )
-        })} */}
-        {/* <Route path="/" element={<Navbar type={"Main"}/>}/> */}
-        {/* <Route path="/" element={<Introduction/>}/> */}
-      {/* </Routes> */}
+      <Flex
+        justify={"center"}
+        maxW={pxToRem(1536)}
+        p={pxToRem(10)}
+        // bg={colorMode === "light"  ? Color.ColorBase.base_200 : Color.ColorBase.base_content}
+        bg={Color.ColorBase.base_200}
+      >
+        <Flex direction={"column"}>
 
-      <Navbar/>
-      <Introduction/>
-      <Projects/>
-      <Experiences/>
+          <Navbar />
+        
+          <Box pt={pxToRem(32)}>
+            <Flex
+              maxW={{
+                base: pxToRem(360),
+                sm: pxToRem(476),
+                md: pxToRem(640),
+                lg: pxToRem(1440),
+              }}
+              pb={pxToRem(32)}
+              gap={{ md: pxToRem(0), lg: pxToRem(20)}}
+            >
+              <Box display={{ base: "none", lg: "block" }}>
+                <SideBar />
+              </Box>
+              <Flex justify={"center"}>
+                <MainSection />
+              </Flex>
+            </Flex>
+          </Box>
+        </Flex>
+      </Flex>
     </ChakraProvider>
   );
 };
