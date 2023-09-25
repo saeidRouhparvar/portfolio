2ad2a31f6fa8e5e04141e9e7d916fd4021732a90
saeid <saeid.rohparvar@gmail.com>
2023-09-25 23:24:42 +0330

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { ChakraProvider, Box, Flex, useColorMode } from "@chakra-ui/react";
+import { ChakraProvider, Box, Flex } from "@chakra-ui/react";
 import SideBar from "./components/sidBar/SideBar";
 import Color from "./components/Color";
 import { pxToRem } from "./components/Utils";
 import MainSection from "./components/mainSection/MainSection";
 import Navbar from "./components/header/Navbar";
+import { useRoutes } from "react-router-dom";
+// import indexRoutes from "./routes/index";
+import About from "./pages/about/About";
+import Services from "./pages/services/Services";
+import Project from "./pages/projects/Project";
+import Blog from "./pages/blog/Blog";
+import Contact from "./pages/contact/Contact";
 
 const App = () => {
   // const {colorMode} = useColorMode()
+
+  let router = useRoutes([
+    { path:'/main', element:<MainSection/>},
+    { path:'/about', element:<About/>},
+    { path: '/services', element: <Services/> },
+    { path: '/projects', element: <Project/> },
+    { path: '/blog', element: <Blog/> },
+    { path: '/contact', element: <Contact/> },
+  ])
+
   return (
+    <>
     <ChakraProvider>
+      
       <Flex
         justify={"center"}
         maxW={pxToRem(1536)}
@@ -17,9 +36,8 @@ const App = () => {
         bg={Color.ColorBase.base_200}
       >
         <Flex direction={"column"}>
-
           <Navbar />
-        
+
           <Box pt={pxToRem(32)}>
             <Flex
               maxW={{
@@ -29,19 +47,24 @@ const App = () => {
                 lg: pxToRem(1440),
               }}
               pb={pxToRem(32)}
-              gap={{ md: pxToRem(0), lg: pxToRem(20)}}
+              gap={{ md: pxToRem(0), lg: pxToRem(20) }}
             >
               <Box display={{ base: "none", lg: "block" }}>
                 <SideBar />
               </Box>
               <Flex justify={"center"}>
-                <MainSection />
+          {router}
+
+                {/* <MainSection /> */}
               </Flex>
             </Flex>
           </Box>
         </Flex>
       </Flex>
     </ChakraProvider>
+
+    </>
+
   );
 };
 
