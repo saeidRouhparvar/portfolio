d6cf413c3ed35d02211301f36bd31d13a75db9ca
saeid <saeid.rohparvar@gmail.com>
2023-09-25 23:24:55 +0330

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { pxToRem } from "./components/Utils";
 import MainSection from "./components/mainSection/MainSection";
 import Navbar from "./components/header/Navbar";
 import { useRoutes } from "react-router-dom";
-// import indexRoutes from "./routes/index";
 import About from "./pages/about/About";
 import Services from "./pages/services/Services";
 import Project from "./pages/projects/Project";
@@ -16,55 +15,48 @@ const App = () => {
   // const {colorMode} = useColorMode()
 
   let router = useRoutes([
-    { path:'/main', element:<MainSection/>},
-    { path:'/about', element:<About/>},
-    { path: '/services', element: <Services/> },
-    { path: '/projects', element: <Project/> },
-    { path: '/blog', element: <Blog/> },
-    { path: '/contact', element: <Contact/> },
-  ])
+    { path: "/main", element: <MainSection /> },
+    { path: "/about", element: <About /> },
+    { path: "/services", element: <Services /> },
+    { path: "/projects", element: <Project /> },
+    { path: "/blog", element: <Blog /> },
+    { path: "/contact", element: <Contact /> },
+  ]);
 
   return (
     <>
-    <ChakraProvider>
-      
-      <Flex
-        justify={"center"}
-        maxW={pxToRem(1536)}
-        p={pxToRem(10)}
-        // bg={colorMode === "light"  ? Color.ColorBase.base_200 : Color.ColorBase.base_content}
-        bg={Color.ColorBase.base_200}
-      >
-        <Flex direction={"column"}>
-          <Navbar />
+      <ChakraProvider>
+        <Flex
+          justify={"center"}
+          maxW={pxToRem(1536)}
+          p={pxToRem(10)}
+          // bg={colorMode === "light"  ? Color.ColorBase.base_200 : Color.ColorBase.base_content}
+          bg={Color.ColorBase.base_200}
+        >
+          <Flex direction={"column"}>
+            <Navbar />
 
-          <Box pt={pxToRem(32)}>
-            <Flex
-              maxW={{
-                base: pxToRem(360),
-                sm: pxToRem(476),
-                md: pxToRem(640),
-                lg: pxToRem(1440),
-              }}
-              pb={pxToRem(32)}
-              gap={{ md: pxToRem(0), lg: pxToRem(20) }}
-            >
-              <Box display={{ base: "none", lg: "block" }}>
-                <SideBar />
-              </Box>
-              <Flex justify={"center"}>
-          {router}
-
-                {/* <MainSection /> */}
+            <Box pt={pxToRem(32)}>
+              <Flex
+                maxW={{
+                  base: pxToRem(360),
+                  sm: pxToRem(476),
+                  md: pxToRem(640),
+                  lg: pxToRem(1440),
+                }}
+                pb={pxToRem(32)}
+                gap={{ md: pxToRem(0), lg: pxToRem(20) }}
+              >
+                <Box display={{ base: "none", lg: "block" }}>
+                  <SideBar />
+                </Box>
+                <Flex justify={"center"}>{router}</Flex>
               </Flex>
-            </Flex>
-          </Box>
+            </Box>
+          </Flex>
         </Flex>
-      </Flex>
-    </ChakraProvider>
-
+      </ChakraProvider>
     </>
-
   );
 };
 
