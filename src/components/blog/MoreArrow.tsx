48574126b999630a194076dd6a315a87fbeec13d
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-10-10 23:55:41 +0330

    add new props

--- src/components/blog/MoreArrow.tsx
+++ src/components/blog/MoreArrow.tsx
@@ -1,15 +1,20 @@
-import { Box, Flex, useColorMode } from '@chakra-ui/react'
+import { Box, BoxProps, Flex, useColorMode } from '@chakra-ui/react'
 import React from 'react'
 import Typography from '../Typography'
 import Color from '../Color';
 import { LittleArrow } from '../Svg';
 import { pxToRem } from '../Utils';
 
-export const MoreArrow = () => {
+interface Interface extends BoxProps {
+  reset?:any
+  onclick?:any
+}
+
+export const MoreArrow = ({onclick,...reset}:Interface) => {
   const { colorMode } = useColorMode();
 
   return (
-    <Flex gap={pxToRem(8)} align={"center"}>
+    <Flex gap={pxToRem(8)} align={"center"} {...reset} >
         <Box {...Typography.b5_medium} color={colorMode === "light" ? Color.ColorPrimary.primary :""}>Read More</Box>
         <Box mt={"4px"}>{LittleArrow}</Box>
     </Flex>
