972eec25939d5a33dcba7c52dbaba232ae36355e
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-10-16 22:16:35 +0330

    create footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,24 +3,43 @@ import React from "react";
 import Color from "../Color";
 import { pxToRem } from "../Utils";
 import Typography from "../Typography";
+import { BigInstagram, FaceBook, Instagram, LinkedIn, Twitter } from "../Svg";
 
 export const Footer = () => {
   const { colorMode } = useColorMode();
   return (
-    <Flex
-      justify={"center"}
-      align={"center"}
-      direction={{base:"column",sm:"row"}}
-      {...Typography.b4_regular}
-      bg={colorMode === "light" ? Color.ColorBase.base_100 : ""}
-      borderRadius={"12px"}
-      px={pxToRem(24)}
-      py={pxToRem(20)}
-    > 
-    <Box>© 2023 SaeidRouhparvar. </Box>
-    <Box>All Rights Reserved.</Box>
-      
-    </Flex>
+    <>
+  
+      <Flex  mt={pxToRem(64)} w={"100%"} justify={"space-around"} gap={pxToRem(24)}>
+        <Flex direction={"column"} gap={pxToRem(20)}>
+          <Box w={"40px"} h={"40px"}>
+            {BigInstagram}
+          </Box>
+          <Box {...Typography.b3_regular}>
+            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit, sed{" "}
+            <br /> do eiusmod tempor incididunt ut <br /> labore et dolore magna
+            aliqua.
+          </Box>
+          <Flex p={"12px 16px"} gap={pxToRem(20)}>
+            <Box onClick={() => {}}>{FaceBook}</Box>
+            <Box>{Instagram}</Box>
+            <Box>{Twitter}</Box>
+            <Box>{LinkedIn}</Box>
+          </Flex>
+        </Flex>
+        <Flex direction={"column"} gap={pxToRem(24)}>
+          <Box>NAVIGATION</Box>
+          <Flex direction={"column"} gap={pxToRem(12)}>
+            <Box>Home</Box>
+            <Box>About</Box>
+            <Box>Projects</Box>
+            <Box>Services</Box>
+            <Box>Blog</Box>
+            <Box>Contact</Box>
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
   );
 };
 export default Footer;
