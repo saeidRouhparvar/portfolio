58b95b4d56679bac9c443dba8a2889ace91afc0e
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-10-27 23:16:16 +0330

    add new svg

--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -430,6 +430,19 @@ const Post = (
   </svg>
 );
 
+const Copy = (
+  <svg
+    width="15"
+    height="15"
+    viewBox="0 0 15 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect x="0.5" y="0.5" width="9" height="9" stroke="#2B2B2B" />
+    <rect x="5.5" y="5.5" width="9" height="9" stroke="#2B2B2B" />
+  </svg>
+);
+
 export {
   FaceBook,
   Instagram,
@@ -448,5 +461,5 @@ export {
   LittleArrow,
   Loc,
   Post,
-  CellPhone18,
+  CellPhone18, Copy
 };
