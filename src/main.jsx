0e1b3e740742a4a45bae8943bfd6b3e900f8f4b8
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-10-29 15:22:56 +0330

    add google auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ColorModeScript } from "@chakra-ui/react";
 import theme from "./components/theme.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
   <>
-  <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <GoogleOAuthProvider clientId="891007400337-4hfq1vnu18ivfj1d5jg24i7qbivb2mad.apps.googleusercontent.com">
+      <BrowserRouter>
+        <App />
+        {/* <Dpp/> */}
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </>
 );
