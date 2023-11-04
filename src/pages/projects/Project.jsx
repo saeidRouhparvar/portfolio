60fd9f9978ad1548f9ff20bbc20e763a01d82e0d
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-11-04 23:47:19 +0330

    refactor project

--- src/pages/projects/Project.jsx
+++ src/pages/projects/Project.jsx
@@ -2,8 +2,26 @@ import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 import SectionHeader from "../../components/SectionHeader";
 import { pxToRem } from "../../components/Utils";
 import ProjectDescriptionCard from "./ProjectDescriptionCard";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import projectDescriptionDatas from "../../components/Data";
+
+const projectData = [
+  {id:1,title:"test",description:"dsfdsfdsfdsfs fdsfds fdsfdsf dsfdsf"},
+  {id:2,title:"test",description:"dsfdsfdsfdsfs fdsfds fdsfdsf dsfdsf"},
+  {id:3,title:"test",description:"dsfdsfdsfdsfs fdsfds fdsfdsf dsfdsf"},
+  {id:4,title:"test",description:"dsfdsfdsfdsfs fdsfds fdsfdsf dsfdsf"},
+  {id:5,title:"test",description:"dsfdsfdsfdsfs fdsfds fdsfdsf dsfdsf"},
+  {id:5,title:"test",description:"dsfdsfdsfdsfs fdsfds fdsfdsf dsfdsf"},
+
+]
 
 const Project = () => {
+
+  // const [projectData,setProjectData] = useState(projectDescriptionDatas)
+
+  // const navigate = useNavigate()
+
   return (
     <Flex
       w={{
@@ -21,31 +39,17 @@ const Project = () => {
           text_2={"dolor do amet sint. Velit officia consequat duis enim velit"}
           text_3={"mollit. lorem ipsum"}
         />
-        <SimpleGrid gap={pxToRem(32)} columns={{base:1,lg:2}}>
-          <ProjectDescriptionCard 
-            title={"Google"}
-            description={"Senior Front End Developer"}
-          />
-          <ProjectDescriptionCard
-            title={"Yahoo"}
-            description={"Mid Level Front End Developer"}
-          />
-          <ProjectDescriptionCard
-            title={"Yahoo"}
-            description={"Mid Level Front End Developer"}
-          />
-          <ProjectDescriptionCard
-            title={"Yahoo"}
-            description={"Mid Level Front End Developer"}
-          />
-          <ProjectDescriptionCard
-            title={"Yahoo"}
-            description={"Mid Level Front End Developer"}
-          />
-          <ProjectDescriptionCard
-            title={"Yahoo"}
-            description={"Mid Level Front End Developer"}
-          />
+        <SimpleGrid gap={pxToRem(32)} columns={{ base: 1, lg: 2 }}>
+          {projectData.map((item) => (
+            <ProjectDescriptionCard
+            // onClick={() => {
+            //   navigate(`/projectSiglePage/${item.id}`)
+            // }}
+              title={item.title}
+              description={item.description}
+              key={item.id}
+            />
+          ))}
         </SimpleGrid>
       </Flex>
     </Flex>
