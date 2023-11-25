04084719ffcaa13401614b9a990ca2d6a648da82
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2023-11-25 23:44:05 +0330

    add typography

---
+++ component/Typography.tsx
@@ -0,0 +1,54 @@
+import { BoxProps } from "@chakra-ui/react"
+
+// interface Interface extends BoxProps {
+//     responsive?:any
+// }
+
+const Typography = ({
+    H1:{fontWeight: 600, fontSize: {lg:"48px",sm:"30px"}, lineHeight: {lg:"56px",sm:"36px"}, fontStyle: "normal",},
+    h1_semiBold :{fontWeight: 600, fontSize: "36px", lineHeight: "40px", fontStyle: "normal",},
+    h1_bold :{fontWeight: 700, fontSize: {base:"24px",sm:"30px",lg:"36px"}, lineHeight: "32px", fontStyle: "normal",},
+
+    h2_semiBold :{fontWeight: 600, fontSize: "30px", lineHeight: "40px", fontStyle: "normal",},
+    h2_bold :{fontWeight: 700, fontSize: "30px", lineHeight: "28px", fontStyle: "normal",},
+
+    h3_semiBold :{fontWeight: 600, fontSize: "24px", lineHeight: "40px", fontStyle: "normal",},
+    h3_bold :{fontWeight: 700, fontSize: "24px", lineHeight: "26px", fontStyle: "normal",},
+
+    h4_semiBold :{fontWeight: 600, fontSize: "20px", lineHeight: "40px", fontStyle: "normal",},
+    h4_bold :{fontWeight: 700, fontSize: "20px", lineHeight: "24px", fontStyle: "normal",},
+
+    h5_semiBold :{fontWeight: 600, fontSize: "18px", lineHeight: "28px", fontStyle: "normal",},
+    h5_bold :{fontWeight: 700, fontSize: "18px", lineHeight: "20px", fontStyle: "normal",},
+
+    h6_semiBold :{fontWeight: 600, fontSize: "16px", lineHeight: "40px", fontStyle: "normal",},
+    h6_bold :{fontWeight: 700, fontSize: "16px", lineHeight: "16px", fontStyle: "normal",},
+
+    b1_regular :{fontWeight: 400, fontSize: "24px", lineHeight: "32px", fontStyle: "normal",},
+    b1_medium :{fontWeight: 500, fontSize: "24px", lineHeight: "32px", fontStyle: "normal",},
+    b1_bold :{fontWeight: 700, fontSize: "24px", lineHeight: "32px", fontStyle: "normal",},
+
+    b2_regular :{fontWeight: 400, fontSize: "20px", lineHeight: "28px", fontStyle: "normal",},
+    b2_medium :{fontWeight: 500, fontSize: "20px", lineHeight: "28px", fontStyle: "normal",},
+    b2_bold :{fontWeight: 700, fontSize: "20px", lineHeight: "28px", fontStyle: "normal",},
+
+    b3_regular :{fontWeight: 400, fontSize: "18px", lineHeight: "26px", fontStyle: "normal",},
+    b3_medium :{fontWeight: 500, fontSize: "18px", lineHeight: "26px", fontStyle: "normal",},
+    b3_bold :{fontWeight: 700, fontSize: "18px", lineHeight: "26px", fontStyle: "normal",},
+
+    b4_regular :{fontWeight: 400, fontSize: {base:"14px",sm:"16px"}, lineHeight: {base:"20px",sm:"24px"}, fontStyle: "normal",},
+    b4_medium :{fontWeight: 500, fontSize: "16px", lineHeight: "24px", fontStyle: "normal",},
+    b4_bold :{fontWeight: 700, fontSize: "16px", lineHeight: "24px", fontStyle: "normal",},
+
+    b5_regular :{fontWeight: 400, fontSize: "14px", lineHeight: "20px", fontStyle: "normal",},
+    b5_medium :{fontWeight: 500, fontSize: "14px", lineHeight: "20px", fontStyle: "normal",},
+    b5_bold :{fontWeight: 700, fontSize: "14px", lineHeight: "20px", fontStyle: "normal",},
+
+    b6_regular :{fontWeight: 400, fontSize: "12px", lineHeight: "16px", fontStyle: "normal",},
+    b6_medium :{fontWeight: 500, fontSize: "12px", lineHeight: "16px", fontStyle: "normal",},
+    b6_bold :{fontWeight: 700, fontSize: "12px", lineHeight: "16px", fontStyle: "normal",},
+
+
+})
+
+export default {...Typography}
